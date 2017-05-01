bc2231612b48fb6ac475a1636556fc3aab2c7e93
bryan <bryan@localhost.localdomain>
2017-05-01 10:51:35 -0500

    POSTY! is working for /wiki

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router();
+const chalk = require('chalk')
 
 router.get('/', function (req, res) {
     res.redirect('/')
 });
 
 router.post('/', function (req, res) {
-    res.sendStatus(201);
+  printo(req.body,"POSTY!");
+  res.json(req.body)
 });
 
 router.get('/add', function (req, res) {
@@ -14,3 +16,15 @@ router.get('/add', function (req, res) {
 });
 
 module.exports = router;
+
+
+function printo(obj,header){
+  var keys = Object.keys(obj);
+  var str = "";
+  keys.forEach( key=>{
+    str += ",\n" + key + ":" + obj[key];
+  });
+  str = header + " {\n" + str.slice(2) + "\n}";
+  console.log(chalk.magenta(str));
+
+}
